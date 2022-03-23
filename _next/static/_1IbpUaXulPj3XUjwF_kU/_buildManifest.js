1a9c1c75a721ee97bca38a0bfad56c8bf8bdacbb
fankimm <crakim7@naver.com>
2022-03-23 21:43:09 +0900

    Add files via upload

---
+++ _next/static/_1IbpUaXulPj3XUjwF_kU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4b34599b136db7dc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
